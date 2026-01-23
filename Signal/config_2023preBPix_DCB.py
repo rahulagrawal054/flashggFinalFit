97c2ab14a7f367cc5de46c5d6eea6ba036643bf8
Rahul Kumar Agrawal <rkumarag@lxplus981.cern.ch>
2026-01-23 09:04:04 +0100

    adding config file for 2023 DCB

---
+++ Signal/config_2023preBPix_DCB.py
@@ -0,0 +1,25 @@
+# Config file: options for signal fitting
+signalScriptCfg = {
+
+  # Setup
+  'inputWSDir':'/eos/user/r/rkumarag/hgg_tth_th_cp_analysis_Bf19Dec/finalFitPreparation/workspaces_03Nov/2023preBPix/ws_signal', # dir storing flashgg workspaces
+  'procs':'auto', # if auto: inferred automatically from filenames (requires names to be of from *pythia8_{PROC}.root)
+  'cats':'auto', # if auto: inferred automatically from (0) workspace
+  #'ext':'earlyAnalysis_freeze_include', # output directory extension
+  'ext':'tth_th_analysis_2023preBPix_DCB', # output directory extension
+  'analysis':'tth_th_analysis', # To specify replacement dataset and XS*BR mapping (defined in ./tools/replacementMap.py and ./tools/XSBRMap.py respectively)
+  'year':'2023preBPix', # Use 'combined' if merging all years: not recommended
+  'massPoints':'125', # You can now run with a single mass point if necessary
+
+  #Photon shape systematics  
+  'scales':'ScaleEE,ScaleEB', # separate nuisance per year
+  'scalesCorr':'Material,FNUF', # correlated across years
+  #'scalesGlobal':'NonLinearity,Geant4', # affect all processes equally, correlated across years
+  # Removed nonLinearity for HIG-23-014 
+  'scalesGlobal':'', # affect all processes equally, correlated across years
+  'smears':'Smearing', # separate nuisance per year
+
+  # Job submission options
+  'batch':'local', # ['condor_lxplus','condor','SGE','IC','local']
+  'queue':'microcentury' # use hep.q for IC
+}
