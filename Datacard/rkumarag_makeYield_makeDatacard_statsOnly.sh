90776360ae45c76bb212ce6a2d525525e2fe5a33
Rahul <rahulkumar.agrawal@niser.ac.in>
2026-02-11 15:03:52 +0100

    change path and update

--- Datacard/rkumarag_makeYield_makeDatacard_statsOnly.sh
+++ Datacard/rkumarag_makeYield_makeDatacard_statsOnly.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-
 # Run yields
 python3 RunYields.py \
   --inputWSDirMap \
-2022preEE=/eos/user/r/rkumarag/hgg_tth_th_cp_analysis/finalFitPreparation/outputForFinalFits_03Nov2025/workspaces/2022preEE/ws_signal,\
-2022postEE=/eos/user/r/rkumarag/hgg_tth_th_cp_analysis/finalFitPreparation/outputForFinalFits_03Nov2025/workspaces/2022postEE/ws_signal,\
-2023preBPix=/eos/user/r/rkumarag/hgg_tth_th_cp_analysis/finalFitPreparation/outputForFinalFits_03Nov2025/workspaces/2023preBPix/ws_signal,\
-2023postBPix=/eos/user/r/rkumarag/hgg_tth_th_cp_analysis/finalFitPreparation/outputForFinalFits_03Nov2025/workspaces/2023postBPix/ws_signal \
+2022preEE=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithSyst/workspaces/2022preEE/ws_signal,\
+2022postEE=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithSyst/workspaces/2022postEE/ws_signal,\
+2023preBPix=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithSyst/workspaces/2023preBPix/ws_signal,\
+2023postBPix=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithSyst/workspaces/2023postBPix/ws_signal\
   --cats auto \
   --procs auto \
-  --ext tth_th_analysis \
+  --ext tth_th_analysis_StatOnly \
   --mergeYears \
   --skipZeroes \
   --batch local
 
-# Make datacard
+# Make Datacard
 python3 makeDatacard.py \
   --years 2022preEE,2022postEE,2023preBPix,2023postBPix \
-  --ext tth_th_analysis \
+  --ext tth_th_analysis_StatOnly \
   --prune \
   --pruneThreshold 0.001 \
   --doTrueYield \
   --analysis tth_th_analysis \
-  --skipCOWCorr \
+  --output Datacard_StatOnly 
 
+#2022preEE=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithOutSyst/workspaces/2022preEE/ws_signal,\
+#2022postEE=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithOutSyst/workspaces/2022postEE/ws_signal,\
+#2023preBPix=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithOutSyst/workspaces/2023preBPix/ws_signal,\
+#2023postBPix=/eos/user/r/rkumarag/OLD_OUTPUT/OutputForFinalFit_WithOutSyst/workspaces/2023postBPix/ws_signal \
