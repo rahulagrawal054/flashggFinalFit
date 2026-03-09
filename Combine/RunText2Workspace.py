ff81467671089a255f7648430ba0aab360923e14
Rahul <rahulkumar.agrawal@niser.ac.in>
2026-03-09 09:36:54 +0100

    change 125 to 125.38

--- Combine/RunText2Workspace.py
+++ Combine/RunText2Workspace.py
@@ -10,7 +10,7 @@ def get_options():
   parser.add_option("--outputName", dest='outputName', default='Datacard', help="Name of the datacard. Can be custom.")
   parser.add_option('--mode', dest='mode', default='mu_inclusive', help="Physics Model (specified in models.py)")
   parser.add_option('--ext',dest='ext', default="", help='In case running over datacard with extension')
-  parser.add_option('--common_opts',dest='common_opts', default="-m 125 higgsMassRange=122,128", help='Common options')
+  parser.add_option('--common_opts',dest='common_opts', default="-m 125.38 higgsMassRange=122,128", help='Common options')
   parser.add_option('--batch', dest='batch', default='condor', help="Batch system [SGE,IC,condor]")
   parser.add_option('--queue', dest='queue', default='workday', help="Condor queue")
   parser.add_option('--ncpus', dest='ncpus', default=4, type='int', help="Number of cpus")
