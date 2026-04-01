f69d4fd37c449228c177a299061a586293010f28
Rahul <rahulkumar.agrawal@niser.ac.in>
2026-04-01 09:24:18 +0200

    modified the config and runner

--- Background/config_ttH_tH_CP.py
+++ Background/config_ttH_tH_CP.py
@@ -3,11 +3,11 @@
 backgroundScriptCfg = {
   
   # Setup
-  #'inputWS':'/eos/user/r/rkumarag/CMSSW_14_1_0_pre4/src/flashggFinalFit/Trees2WS/ws/allData.root',
-  'inputWS':'/eos/user/r/rkumarag/OLD_OUTPUT/outputForFinalFits_03Nov2025/workspaces/Data/ws/allData.root', # location of 'allData.root' file
+  'inputWS':'/eos/user/r/rkumarag/CMSSW_14_1_0_pre4/src/flashggFinalFit/Trees2WS/ws/allData.root',
+  #'inputWS':'/eos/user/r/rkumarag/OLD_OUTPUT/outputForFinalFits_03Nov2025/workspaces/Data/ws/allData.root', # location of 'allData.root' file
   'cats':'ttH_had_1', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
-  'ext':'tth_th_analysis_ttH_had_1', # extension to add to output directory
+  'ext':'tth_th_analysis', # extension to add to output directory
   'year':'combined', # Use combined when merging all years in category (for plots)
 
   # Job submission options
--- Background/rkumarag_fTest.sh
+++ Background/rkumarag_fTest.sh
@@ -3,6 +3,8 @@
 CONFIG_FILE=$1
 LOG_FILE="ftest_log_$(date +%Y%m%d_%H%M%S).log"
 
+START_TIME=$(date +%s)
+
 python3 RunBackgroundScripts.py --inputConfig "$CONFIG_FILE" --mode fTestParallel > "$LOG_FILE" 2>&1 &
 
 PID=$!
@@ -13,5 +15,22 @@ echo "Logs: $LOG_FILE"
 # Wait for process to finish
 wait $PID
 
+END_TIME=$(date +%s)
+ELAPSED=$((END_TIME - START_TIME))
+
 echo "Job completed for config: $CONFIG_FILE"
-echo "Check logs: $LOG_FILE"
+echo "Time taken: ${ELAPSED} seconds"
+#echo "Processing log..."
+
+# Remove unwanted lines (INFO, Info, ERROR, blank)
+#sed -i '/INFO/d;/Info/d;/ERROR/d;/^$/d' "$LOG_FILE"
+
+# Extract "Adding to Envelope" lines
+#echo "---- Envelope Lines ----"
+#grep -n "Adding to Envelope" "$LOG_FILE"
+
+# Check for segmentation fault
+#echo "---- Error Check ----"
+#grep -i "segmentation fault" "$LOG_FILE" && echo "⚠️ Segmentation fault found!" || echo "No segmentation fault"
+
+#echo "Done. Cleaned log: $LOG_FILE"
