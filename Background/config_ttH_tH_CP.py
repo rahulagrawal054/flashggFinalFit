017c64c763e8120ec328df568fa3ad8e786972ba
Rahul <rahulkumar.agrawal@niser.ac.in>
2026-04-01 11:49:28 +0200

    adding ws path of the config file

--- Background/config_ttH_tH_CP.py
+++ Background/config_ttH_tH_CP.py
@@ -4,8 +4,7 @@ backgroundScriptCfg = {
   
   # Setup
   'inputWS':'/eos/user/r/rkumarag/CMSSW_14_1_0_pre4/src/flashggFinalFit/Trees2WS/ws/allData.root',
-  #'inputWS':'/eos/user/r/rkumarag/OLD_OUTPUT/outputForFinalFits_03Nov2025/workspaces/Data/ws/allData.root', # location of 'allData.root' file
-  'cats':'ttH_had_1', # auto: automatically inferred from input ws
+  'cats':'auto', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
   'ext':'tth_th_analysis', # extension to add to output directory
   'year':'combined', # Use combined when merging all years in category (for plots)
